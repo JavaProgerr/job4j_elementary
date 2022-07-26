900e7b5d5064ddddc7bbea7bf446412877b19783
JavaProgerr <ligtor2010@gmail.com>
2022-07-26 22:47:04 +0300

    Таблица умножения на 1

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Oleg Kostenetsky");
+        System.out.println("08.10.1988");
+    }
+}
