fc10a3385f441a85613a6450d235cc8e307fa4ec
JavaProgerr <ligtor2010@gmail.com>
2022-10-19 22:18:44 +0300

    Простое число [#171694 #353038]

--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -6,7 +6,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CheckPrimeNumberTest {
     @Test
     public void when5() {
-        int number = 101;
+        int number = 5;
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isTrue();
     }
